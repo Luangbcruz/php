6e9f965a8f462198bdcfd4eb73d140ffd83fbbab
luangbcruz <luangabrielbezerradacruz225@gmail.com>
2025-04-12 12:15:53 -0300

    first commit

---
+++ hello-world/helloworld.php
@@ -0,0 +1,9 @@
+<?php
+$Firstname= 'Eduardo';
+
+echo '<h1 style= "color:green">hello, world!</h1>';
+
+echo 'dados pessoais:';
+echo $Firstname;
+
+?>
---
+++ hello-world/index2.php
@@ -0,0 +1,13 @@
+<?php
+$Firstname= 'Eduardo';
+$lastName= 'bueno';
+echo 'Hello, world ';
+echo $Firstname;
+echo '';
+echo $lastName;
+
+
+echo "hello, world, $firtstName.' '.$lastName";
+
+
+?>
